e3acda1b684d12a72e53e5331ec802a294788f1f
JeddAlcalde <jeddalcalde@gmail.com>
2022-08-11 16:39:42 -0400

    Change approach to hover event listener

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let container = document.querySelector(".largeDiv");
+const container = document.querySelector(".largeDiv");
 
 for(let i = 0; i < 16; i++){
     let smallBox = document.createElement("div");
     smallBox.classList.add("gridbox");
     smallBox.setAttribute("id", i.toString());
+    console.log(i.toString());
     smallBox.textContent = "This is a gridbox";
+    
+    smallBox.addEventListener("mouseover" ,function(e){
+        console.log(smallBox.id);
+        smallBox.classList.add("gridbox-hover");
+        smallBox.innerText = "hovered";
+    })
+
     container.appendChild(smallBox);
 }
-
-let gridList = document.querySelectorAll("gridbox");
-
-gridList.forEach(gridSquare => {
-    gridSquare.addEventListener("mouseover", function(e){
-        console.log(gridSquare.id)
-        gridSquare.classList.add("gridbox-hover");
-        gridSquare.innerText = "Hovered";
-    })
-});
