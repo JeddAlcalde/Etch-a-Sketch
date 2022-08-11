98eaec33524093213433d88629d4f5994929d160
JeddAlcalde <jeddalcalde@gmail.com>
2022-08-11 16:29:08 -0400

    Hover Approach Change
    Instead of changing the css we are now
    altering the CSS classes through the
    JavaScript.

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let container = document.querySelector(".largeDiv");
 for(let i = 0; i < 16; i++){
     let smallBox = document.createElement("div");
     smallBox.classList.add("gridbox");
+    smallBox.setAttribute("id", i.toString());
     smallBox.textContent = "This is a gridbox";
-    container.appendChild(smallBox)
+    container.appendChild(smallBox);
 }
+
+let gridList = document.querySelectorAll("gridbox");
+
+gridList.forEach(gridSquare => {
+    gridSquare.addEventListener("mouseover", function(e){
+        console.log(gridSquare.id)
+        gridSquare.classList.add("gridbox-hover");
+        gridSquare.innerText = "Hovered";
+    })
+});
