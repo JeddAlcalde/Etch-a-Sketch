0965ca06ac11f2a0b66c648babfddac6529f7f48
JeddAlcalde <jeddalcalde@gmail.com>
2022-08-11 13:52:04 -0400

    Script tag now appends child divs

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let container = document.querySelector(".largeDiv");
 
-let smallBox = document.createElement("div");
-smallBox.classList.add("gridbox");
-smallBox.textContent = "This is a gridbox";
+for(let i = 0; i < 16; i++){
+    let smallBox = document.createElement("div");
+    smallBox.classList.add("gridbox");
+    smallBox.textContent = "This is a gridbox";
+    container.appendChild(smallBox)
+}
