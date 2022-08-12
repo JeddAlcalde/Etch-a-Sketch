be2ad4208989a576d282ab080ed5a10c7976ef98
JeddAlcalde <jeddalcalde@gmail.com>
2022-08-12 13:04:59 -0400

    Restart Button Finished
    style.css changes made gridBoxes smaller to accomodate
    more gridBoxes in the container. script.js changes made
    restart function for button work correctly.

--- script.js
+++ script.js
@@ -2,42 +2,34 @@ const container = document.querySelector(".largeDiv");
 
 const restart = document.createElement("button");
 restart.innerText = "Restart";
-restart.addEventListener("click", function(e){
+restart.addEventListener("click", function(e) {
     //prompts user to give a number
-    let newGridCount = prompt("How many squares would you like per side? (Less than 100)").parseInt();
+    let newGridCount = parseInt(prompt("How many squares would you like per side? (Less than 100)"));
+    console.log(newGridCount);
 
     //loop to "erase all grids"
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
 
-    for(let i = 0; i < newGridCount; i++){
+    newGame(newGridCount);
+    container.append(restart);
+})
+
+function newGame(gridCount){
+    for(let i = 0; i < gridCount; i++){
         let smallBox = document.createElement("div");
         smallBox.classList.add("gridbox");
-        smallBox.setAttribute("id", newGridCount.toString());
-        console.log(i.toString());
+        smallBox.setAttribute("id", i.toString());
         
         smallBox.addEventListener("mouseover" ,function(e){
-            console.log(smallBox.id);
             smallBox.classList.add("gridbox-hover");
         })
     
         container.appendChild(smallBox);
     }
-})
-container.insertBefore(restart);
-
-
-for(let i = 0; i < 16; i++){
-    let smallBox = document.createElement("div");
-    smallBox.classList.add("gridbox");
-    smallBox.setAttribute("id", i.toString());
-    console.log(i.toString());
-    
-    smallBox.addEventListener("mouseover" ,function(e){
-        console.log(smallBox.id);
-        smallBox.classList.add("gridbox-hover");
-    })
-
-    container.appendChild(smallBox);
 }
+
+newGame(448);
+
+container.append(restart);
